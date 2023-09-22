0359d5f6d2f556ddcf7f2ec99e4ef7d99570609a
guptaji1008 <132588314+guptaji1008@users.noreply.github.com>
2023-09-22 18:46:49 +0530

    final (i think)

--- src/components/BookMovie.js
+++ src/components/BookMovie.js
@@ -138,10 +138,9 @@ export default function BookMovie(props) {
 
             if (summaryBooking.movieName !== "" &&
                 summaryBooking.timing !== "" &&
-                (summaryBooking.A1 >= 0 && summaryBooking.A2 >= 0 && summaryBooking.A3 >= 0 && summaryBooking.A4 >= 0 && summaryBooking.D1 >= 0 && summaryBooking.D2 >= 0  &&
-                    (summaryBooking.A1+summaryBooking.A2+summaryBooking.A3+summaryBooking.A4+summaryBooking.D1+summaryBooking.D2) > 0
+                (summaryBooking.A1 >= 0 && summaryBooking.A2 >= 0 && summaryBooking.A3 >= 0 && summaryBooking.A4 >= 0 && summaryBooking.D1 >= 0 && summaryBooking.D2 >= 0
                     ) && (
-                    seatA1 >= 0 && seatA2 >= 0 && seatA3 >= 0 && seatA4 >= 0 && seatD1 >= 0 && seatD2 > 0 && (seatA1+seatA2+seatA3+seatA4+seatD1+seatD2) > 0
+                    seatA1 >= 0 && seatA2 >= 0 && seatA3 >= 0 && seatA4 >= 0 && seatD1 >= 0 && seatD2 >= 0
                 )
             ) {
 
@@ -206,6 +205,7 @@ export default function BookMovie(props) {
 
     }, [toggle])
 
+    // converting into date format :
     const formatDateToDdMmYy = (date) => {
         let newDate = date.slice(0, 10)
         return newDate
@@ -275,7 +275,7 @@ export default function BookMovie(props) {
                             <h3>Last Booking Status : </h3>
                             {
                                 lastBookingHistory.map((elem, index) => {
-                                    return <div className='inside-previous-record'>
+                                    return <div className='inside-previous-record' key={index}>
                                         <div>
                                             <div style={{ fontWeight: "bold" }}>Seat : </div>
                                             <p>A1: {elem.A1}</p>
